3af9258458af06e054264b27a715f09e3630d414
Thiago Castoldi <castoldithiago4@gmail.com>
2025-05-01 17:00:13 -0300

    distribucion uniforme[a,b] y funciones mostrar

--- TrabajoSimTP2.py
+++ TrabajoSimTP2.py
@@ -4,6 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+def mostrarRandoms(randoms):
+    mostrar = input("¿Desea mostrar los numeros aleatorios generados? (s/n): ").lower()
+    if mostrar == "s":
+        print("Numeros aleatorios generados:")
+        for i in range(len(randoms)):
+            print(f"{i+1}: {randoms[i]}")
+        print("-----------------------------------")
+
+def mostrarTablaFrecuencias(randoms, intervalos):
+    frec, bordes = np.histogram(randoms, bins=intervalos)
+    inter = [f"{round(bordes[i], 4)} - {round(bordes[i+1], 4)}" for i in range(len(bordes)-1)]
+
+    tabla = pd.DataFrame({
+        'Intervalo': inter,
+        'Frecuencia': frec
+    })
+
+    print(tabla)
+    print("-----------------------------------")
+
+def mostrarHistograma(randoms, intervalos):
+    plt.hist(randoms, bins=intervalos, edgecolor='black')
+    plt.title("Histograma")
+    plt.xlabel("Intervalos")
+    plt.ylabel("Frecuencia")
+    plt.grid(True)
+    plt.show()
+
+
+# Trabajo Practico 2 - Generacion de numeros aleatorios
 print("TP2 generacion de numeros aleatorios")
 
 opcion = -1
@@ -30,32 +60,29 @@ def uniforme():
     if muestra > 1000000:
         muestra = int(input("Ingresar muestra menor o igual a 1 millon: "))
 
+    a = float(input("Ingrese el valor de a para la distribución uniforme[a,b]: "))
+    b = float(input("Ingrese el valor de b para la distribución uniforme[a,b]: "))
+
 
     print(frecuencias)
     cantidad = int(input("Ingrese su eleccion: "))
 
     intervalos = frecuencia(cantidad)
 
-    randoms = [round(random.random(), 4) for i in range(muestra)]
+    randoms = []
+    for i in range (muestra):
+        rnd = random.random()
+        x = a + (b-a)*rnd
+        randoms.append(round(x,4))
+
+    # Randoms generados (opcional)
+    mostrarRandoms(randoms)
 
     # Tabla de frecuencias
-    frec, bordes = np.histogram(randoms, bins=intervalos)
-    inter = [f"{round(bordes[i], 4)} - {round(bordes[i+1], 4)}" for i in range(len(bordes)-1)]
+    mostrarTablaFrecuencias(randoms, intervalos)
 
-    tabla = pd.DataFrame({
-        'Intervalo': inter,
-        'Frecuencia': frec
-    })
-
-    print(tabla)
-
-        # Histograma
-    plt.hist(randoms, bins=intervalos, edgecolor='black')
-    plt.title("Histograma - Distribución Uniforme")
-    plt.xlabel("Intervalos")
-    plt.ylabel("Frecuencia")
-    plt.grid(True)
-    plt.show()
+    # Histograma
+    mostrarHistograma(randoms, intervalos)
 
 
 
@@ -79,24 +106,14 @@ def exponencial():
         x = -math.log(1-rnd) / lambda_val
         randoms.append(round(x,4))
 
+    # Randoms generados (opcional)
+    mostrarRandoms(randoms)
+
     # Tabla de frecuencias
-    frec, bordes = np.histogram(randoms, bins=intervalos)
-    inter = [f"{round(bordes[i], 4)} - {round(bordes[i+1], 4)}" for i in range(len(bordes)-1)]
-
-    tabla = pd.DataFrame({
-        'Intervalo': inter,
-        'Frecuencia': frec
-    })
-
-    print(tabla)
+    mostrarTablaFrecuencias(randoms, intervalos)
 
     # Histograma
-    plt.hist(randoms, bins=intervalos, edgecolor='black')
-    plt.title("Histograma - Distribución exponencial")
-    plt.xlabel("Intervalos")
-    plt.ylabel("Frecuencia")
-    plt.grid(True)
-    plt.show()
+    mostrarHistograma(randoms, intervalos)
 
 def normal():
 
@@ -123,28 +140,17 @@ def normal():
         randoms.append(round(n1, 4))
         randoms.append(round(n2, 4))
 
-        # Tabla de frecuencias
-    frec, bordes = np.histogram(randoms, bins=intervalos)
-    inter = [f"{round(bordes[i], 4)} - {round(bordes[i+1], 4)}" for i in range(len(bordes)-1)]
+    # Randoms generados (opcional)
+    mostrarRandoms(randoms)
 
-    tabla = pd.DataFrame({
-        'Intervalo': inter,
-        'Frecuencia': frec
-    })
-
-    print(tabla)
+    # Tabla de frecuencias
+    mostrarTablaFrecuencias(randoms, intervalos)
 
     # Histograma
-    plt.hist(randoms, bins=intervalos, edgecolor='black')
-    plt.title("Histograma - Distribución normal")
-    plt.xlabel("Intervalos")
-    plt.ylabel("Frecuencia")
-    plt.grid(True)
-    plt.show()
-
+    mostrarHistograma(randoms, intervalos)
 
 menu = " Seleccionar distribucion: \n" \
-"       1 - Uniforme \n" \
+"       1 - Uniforme [a,b] \n" \
 "       2 - exponencial \n" \
 "       3 - normal \n" \
 "       0 - salir"
